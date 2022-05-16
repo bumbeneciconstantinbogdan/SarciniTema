aa9ef62700cc8d0c8b8b98a5050ff903ebef8216
Constantin-Bogdan Bumbeneci <bumbenecibogdan35@gmail.com>
2022-05-16 08:06:19 +0300

    Before day

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args) {
+        Integer[][] x = new Integer[6][6];
+        for(int i = 0 ; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+              System.out.println(x[i][j]);
+            }
+        }
+    }
+}
